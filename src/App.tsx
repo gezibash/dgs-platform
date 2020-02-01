ad94914cdd26fa71ed42fcd92a13a557f0f946ed
Gezim Basha <gezimmb@gmail.com>
2020-02-01 11:19:08 +0100

    Added Material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from "./components/Header";
+import Content from "./components/Content";
+
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Header />
+    <Content />
+  </div>;
 };
 
 export default App;
