01b274d4443b3132ef151f8534a5647569128f91
Gezim Basha <gezimmb@gmail.com>
2020-02-01 14:21:21 +0100

    changes to design

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./style.css";
+
+const Content: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="block h-px w-full bg-gray-400">
+      </div>
+    </div>
+  )
+}
+
+export default Content;
